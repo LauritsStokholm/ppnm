e52cdb8b8b96393b79df33be89e82f806918162c
LauritsStokholm <laurits.stokholm@gmail.com>
2021-02-10 11:16:39 +0100

    Adding math.m

--- 1-hello_world/hello.c
+++ 1-hello_world/hello.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <tgmath.h>
 
 int
 main (int argc, char **argv)
 {
+  double x = sqrt(2.0);
   printf("Hello, World!\n");
+  printf("Look! A number: %lg\n", x);
   return 0;
 }
 
